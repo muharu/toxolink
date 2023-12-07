b3be710abce5494f4a6215d2a8bb66dd0b2ed81d
muharu <muharu@proton.me>
2023-12-07 18:41:14 +0700

    feat(database): add schema & types for linktree

---
+++ src/server/db/types.ts
@@ -0,0 +1,16 @@
+import type { ColumnType } from "kysely"
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>
+export type Timestamp = ColumnType<Date, Date | string, Date | string>
+
+export type Linktree = {
+  id: Generated<number>
+  name: string
+  url: string
+  created_at: Generated<Timestamp>
+  updated_at: Timestamp
+}
+export type DB = {
+  linktree: Linktree
+}
